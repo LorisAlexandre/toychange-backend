05a039e12dee1f60520c35a50ed5f6c3cc490ab8
houssemmansour <mansourhoussem1982@gmail.com>
2023-12-09 20:51:58 +0100

    connection shema user et route signup/signin

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const cors = require("cors");
 const fileUpload = require("express-fileupload");
 
 var indexRouter = require("./routes/index");
+var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,5 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+app.use('/users', usersRouter);
 
 module.exports = app;
