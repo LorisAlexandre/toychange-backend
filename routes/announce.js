684fd92aadd99556003cdd835bb6367796462be6
Loris ALEXANDRE <loris.alexandre66@gmail.com>
2023-12-15 19:09:37 +0100

    route upload images

--- routes/announce.js
+++ routes/announce.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const Announce = require("../models/annonce");
-const ObjectId = require("mongodb").ObjectID;
 const router = express.Router();
+const fs = require("fs");
+const PDFDocument = require("pdfkit");
+const cloudinary = require("cloudinary").v2;
+const uniqid = require("uniqid");
 
 // Route to create a new announce
 router.post("/addAnnounce", (req, res) => {
+  let latitude;
+  let longitude;
   const {
     title,
     type,
@@ -23,33 +28,35 @@ router.post("/addAnnounce", (req, res) => {
   )
     .then((res) => res.json())
     .then((data) => {
-      const newAnnounce = new Announce({
-        title,
-        type,
-        deliveryMethod,
-        address: {
-          ...address,
-          coords: {
-            latitude: data[0].lat,
-            longitude: data[0].lon,
-          },
-        },
-        images,
-        category,
-        condition,
-        description,
-        exchangeProposal,
-        donor,
-      });
-
-      newAnnounce.save().then((data) => {
-        if (data) {
-          res.json({ result: true, data });
-        } else {
-          res.json({ error: "server error while creating the announce" });
-        }
-      });
+      latitude = data[0].lat;
+      longitude = data[0].lon;
     });
+  const newAnnounce = new Announce({
+    title,
+    type,
+    deliveryMethod,
+    address: {
+      ...address,
+      coords: {
+        latitude,
+        longitude,
+      },
+    },
+    images,
+    category,
+    condition,
+    description,
+    exchangeProposal,
+    donor,
+  });
+
+  newAnnounce.save().then((data) => {
+    if (data) {
+      res.json({ result: true, data });
+    } else {
+      res.json({ error: "server error while creating the announce" });
+    }
+  });
 });
 
 // Route to delete an announce
@@ -180,6 +187,65 @@ router.get("/nearby", (req, res) => {
       .sort((a, b) => a[0] - b[0]);
     res.json({ result: true, announcesSorted });
   });
+});
+
+router.put("/uploadImages/:id", async (req, res) => {
+  const { id } = req.params;
+  const photos = req.files.photosFromFront;
+  const imagesUrl = [];
+
+  if (photos.length) {
+    const uploadPhoto = (photo) => {
+      return new Promise((resolve, reject) => {
+        cloudinary.uploader
+          .upload_stream(
+            {
+              resource_type: "auto",
+            },
+            (error, result) => {
+              if (error) {
+                reject(error);
+              } else {
+                imagesUrl.push(result.secure_url);
+                resolve(result.secure_url);
+              }
+            }
+          )
+          .end(photo.data);
+      });
+    };
+    Promise.all(photos.map(uploadPhoto))
+      .then((uploadedUrls) => {
+        Announce.updateOne({ _id: id }, { images: uploadedUrls }).then(() => {
+          Announce.findById(id).then((announce) => {
+            res.json({ result: true, announce });
+          });
+        });
+      })
+      .catch((error) => {
+        res.status(500).json({ result: false, error });
+      });
+  } else {
+    cloudinary.uploader
+      .upload_stream(
+        {
+          resource_type: "auto",
+        },
+        (error, result) => {
+          if (error) {
+            res.status(500).json({ result: false, error });
+          } else {
+            imagesUrl.push(result.secure_url);
+            Announce.updateOne({ _id: id }, { images: imagesUrl }).then(() => {
+              Announce.findById(id).then((announce) => {
+                res.json({ result: true, announce });
+              });
+            });
+          }
+        }
+      )
+      .end(photos.data);
+  }
 });
 
 module.exports = router;
