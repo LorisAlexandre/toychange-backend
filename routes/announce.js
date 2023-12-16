ff296fd6d6f84601ef19600191fb8f5bbd5b6048
Loris ALEXANDRE <loris.alexandre66@gmail.com>
2023-12-16 21:56:37 +0100

    route addAnnounce changes

--- routes/announce.js
+++ routes/announce.js
@@ -19,6 +19,8 @@ router.post("/addAnnounce", (req, res) => {
     description,
     exchangeProposal,
     donor,
+    weight,
+    favImage,
   } = req.body;
 
   fetch(
@@ -45,6 +47,8 @@ router.post("/addAnnounce", (req, res) => {
         description,
         exchangeProposal,
         donor,
+        weight,
+        favImage,
       });
 
       newAnnounce.save().then((announce) => {
